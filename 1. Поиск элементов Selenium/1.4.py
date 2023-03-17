1a96016257d542b6c01332bc317b841e813e65a0
Keeper <virys2@gmail.com>
2023-03-17 19:13:04 +0300

    Работаем с основным окном браузера

---
+++ 1. Поиск элементов Selenium/1.4.py
@@ -0,0 +1,12 @@
+# with/as
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+with webdriver.Chrome() as browser:
+    browser.get('http://parsinger.ru/html/watch/1/1_1.html')
+    button = browser.find_element(By.ID, "sale_button")
+    time.sleep(2)
+    button.click()
+    time.sleep(2)
