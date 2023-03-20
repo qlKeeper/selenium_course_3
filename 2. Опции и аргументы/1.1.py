0d26d1c4034e8a1b6bcc796f3c99f0132829efb8
Keeper <virys2@gmail.com>
2023-03-20 12:10:16 +0300

    Запуск браузера с расширениями

---
+++ 2. Опции и аргументы/1.1.py
@@ -0,0 +1,13 @@
+# Запуск браузера с расширениями
+
+
+import time
+from selenium import webdriver
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_extension('coordinates.crx')
+
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = 'https://yandex.ru/'
+    browser.get(url)
+    time.sleep(50)
